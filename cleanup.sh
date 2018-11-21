5806193de6126e49e105a2f633099b07907c6197
Paul Bailey <baileyp@gmail.com>
2018-11-20 21:29:32 -0800

    Adding burning ship fractal algo.

--- cleanup.sh
+++ cleanup.sh
@@ -15,7 +15,7 @@ rm -f test-driver
 rm -f build/ltmain.sh
 rm -f build/test-driver
 
-for dir in . lib mandelbrot random examples
+for dir in . lib mandelbrot random examples burnship1
 do
     rm -f ${dir}/*.in
     rm -f ${dir}/Makefile
