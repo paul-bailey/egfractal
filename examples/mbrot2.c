cdbfcd177f672692d98c6a60b0fdf33969074b3a
Paul Bailey <baileyp@gmail.com>
2018-11-22 09:50:24 -0800

    Fix problems with Buddhabrot, except zoom.

--- examples/mbrot2.c
+++ examples/mbrot2.c
@@ -8,26 +8,27 @@
 
 static struct gbl_t {
         Pxbuf *pxbuf;
-        int n_iteration;
-        bool dither;
+        int n_red;
+	int n_blue;
+	int n_green;
         int height;
         int width;
-        int pallette;
         double zoom_pct;
+	double zoom_pct_inv;
         double zoom_xoffs;
         double zoom_yoffs;
         unsigned long points;
 } gbl = {
         .pxbuf = NULL,
-        .n_iteration = 1000,
-        .dither = false,
-        .height = 3000,
-        .width = 3000,
-        .pallette = 1,
+        .n_red = 5000,
+	.n_blue = 500,
+	.n_green = 50,
+        .height = 600,
+        .width = 600,
         .zoom_pct = 1.0,
         .zoom_xoffs = 0.0,
         .zoom_yoffs = 0.0,
-        .points = 50000000,
+        .points = 500000,
 };
 
 static void
@@ -35,6 +36,16 @@ oom(void)
 {
         fprintf(stderr, "OOM!\n");
         exit(1);
+}
+
+static void
+graph_to_pixel(double x, double y, unsigned int *px, unsigned int *py)
+{
+	static const double third = 1.0 / 3.0;
+	x = (double)gbl.width * ((x + 2.0) * third * gbl.zoom_pct_inv + gbl.zoom_xoffs) + 0.5;
+	y = (double)gbl.height * ((y + 1.5) * third * gbl.zoom_pct_inv + gbl.zoom_yoffs) + 0.5;
+	*px = (int)x;
+	*py = (int)y;
 }
 
 static void
@@ -47,12 +58,8 @@ iterate_r(double x0, double y0, unsigned long *buf, int n, bool isdivergent)
                 double xnew = x * x - y * y + x0;
                 double ynew = 2 * x * y + y0;
                 if (isdivergent && i > 3) {
-                        /*
-                         * XXX REVISIT: It should be fairly easy to apply zoom
-                         * here.
-                         */
-                        unsigned int drawnx = (int)((double)gbl.width * (xnew + 2.0) / 3.0 + 0.5);
-                        unsigned int drawny = (int)((double)gbl.height * (ynew + 1.5) / 3.0 + 0.5);
+			unsigned int drawnx, drawny;
+			graph_to_pixel(xnew, ynew, &drawnx, &drawny);
                         if (drawnx < gbl.width && drawny < gbl.height) {
                                 buf[drawny * gbl.height + drawnx]++;
                         }
@@ -95,10 +102,16 @@ normalize(unsigned long *buf)
 static void
 mbrot2(void)
 {
-        static const int n[3] = { 5000, 500, 50 };
-        int i, row, col;
+        int n[3] = { gbl.n_red, gbl.n_green, gbl.n_blue };
+        int i, row, col, npct;
+	unsigned long onepct = gbl.points / 100;
+	unsigned long pctcount;
         unsigned long *buffer;
         unsigned long *chanbuf[3];
+
+	/* Initialize this */
+	gbl.zoom_pct_inv = 1.0 / gbl.zoom_pct;
+
         buffer = malloc(gbl.width * gbl.height * sizeof(*buffer) * 3);
         if (!buffer)
                 oom();
@@ -107,13 +120,22 @@ mbrot2(void)
         chanbuf[1] = &buffer[gbl.width * gbl.height];
         chanbuf[2] = &buffer[gbl.width * gbl.height * 2];
 
+	pctcount = npct = 0;
+	printf("Progress     ");
         for (i = 0; i < gbl.points; i++) {
-                double x = (double)drand48() * 3.0 - 2.0;
-                double y = (double)drand48() * 3.0 - 1.5;
+                double x = (drand48() * gbl.zoom_pct + gbl.zoom_xoffs) * 3.0 - 2.0;
+                double y = (drand48() * gbl.zoom_pct + gbl.zoom_yoffs) * 3.0 - 1.5; 
                 int chan;
+		if (pctcount++ == onepct) {
+			npct++;
+			pctcount = 0;
+			printf("\e[3D%3d", npct);
+			fflush(stdout);
+		}
                 for (chan = 0; chan < 3; chan++)
                         iterate(x, y, chanbuf[chan], n[chan]);
         }
+	putchar('\n');
 
         /* TODO: Need to blur rasters, perhaps by convolving with 1,1,1... */
         normalize(chanbuf[0]);
@@ -147,10 +169,69 @@ mbrot2(void)
         free(buffer);
 }
 
+static void
+usage(void)
+{
+	fprintf(stderr, "Bad arg\n");
+	exit(EXIT_FAILURE);
+}
+
 int
 main(int argc, char **argv)
 {
-        FILE *fp = fopen("mbrot2.bmp", "wb");
+	FILE *fp;
+	char *endptr;
+	int opt;
+	while ((opt = getopt(argc, argv, "z:x:y:w:h:r:g:b:p:")) != -1) {
+		switch (opt) {
+		case 'z':
+			gbl.zoom_pct = strtod(optarg, &endptr);
+			if (endptr == optarg)
+				usage();
+			break;
+		case 'x':
+			gbl.zoom_xoffs = strtod(optarg, &endptr);
+			if (endptr == optarg)
+				usage();
+			break;
+		case 'y':
+			gbl.zoom_yoffs = strtod(optarg, &endptr);
+			if (endptr == optarg)
+				usage();
+			break;
+		case 'w':
+			gbl.width = strtoul(optarg, &endptr, 0);
+			if (endptr == optarg)
+				usage();
+			break;
+		case 'h':
+			gbl.height = strtoul(optarg, &endptr, 0);
+			if (endptr == optarg)
+				usage();
+			break;
+		case 'r':
+			gbl.n_red = strtoul(optarg, &endptr, 0);
+			if (endptr == optarg)
+				usage();
+			break;
+		case 'g':
+			gbl.n_blue = strtoul(optarg, &endptr, 0);
+			if (endptr == optarg)
+				usage();
+			break;
+		case 'b':
+			gbl.n_green = strtoul(optarg, &endptr, 0);
+			if (endptr == optarg)
+				usage();
+			break;
+		case 'p':
+			gbl.points = strtoul(optarg, &endptr, 0);
+			if (endptr == optarg)
+				usage();
+			break;
+		}
+	}
+        fp = fopen("mbrot2.bmp", "wb");
         if (!fp) {
                 fprintf(stderr, "Cannot open output file\n");
                 return 1;
---
+++ examples/mbrot2.sh
@@ -0,0 +1,6 @@
+# This takes for friggin-ever
+# ./mbrot2.exe  -r1000 -g1000 -b100 -p5000000000 -w6000 -h6000
+
+# grayscale, fewer points
+./mbrot2 -r10000 -g10000 -b10000 -p900000000 -w3000 -h3000
+
