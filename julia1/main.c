ae2420a3719ad3f2d570efdc6de50f1d88ab8f7b
pbailey <>
2018-11-27 11:49:59 -0800

    Fix tab/space dispute in julia1 code.

--- julia1/main.c
+++ julia1/main.c
@@ -10,15 +10,15 @@
 static const double pi = 3.14159265359;
 
 struct gbl_t gbl = {
-	.pxbuf = NULL,
-	.n_iteration = 1000,
-	.dither = false,
-	.height = 600,
-	.width = 600,
-	.pallette = 2,
-	.zoom_pct = 1.0,
-	.zoom_xoffs = 0.0,
-	.zoom_yoffs = 0.0,
+        .pxbuf = NULL,
+        .n_iteration = 1000,
+        .dither = false,
+        .height = 600,
+        .width = 600,
+        .pallette = 2,
+        .zoom_pct = 1.0,
+        .zoom_xoffs = 0.0,
+        .zoom_yoffs = 0.0,
         .cx = -0.70176,
         .cy = -0.3842,
 };
@@ -27,15 +27,15 @@ struct gbl_t gbl = {
 static void
 oom(void)
 {
-	fprintf(stderr, "OOM!\n");
-	exit(EXIT_FAILURE);
+        fprintf(stderr, "OOM!\n");
+        exit(EXIT_FAILURE);
 }
 
 static void
 usage(void)
 {
-	fprintf(stderr, "Bad arg\n");
-	exit(EXIT_FAILURE);
+        fprintf(stderr, "Bad arg\n");
+        exit(EXIT_FAILURE);
 }
 
 /* scale pixels to points of mandelbrot set and handle zoom. */
@@ -57,131 +57,131 @@ xy_to_complex(int row, int col)
 static double
 julia_px(int row, int col)
 {
-	double ret;
-	unsigned long i;
+        double ret;
+        unsigned long i;
         complex_t z;
         complex_t c = { .re = gbl.cx, .im = gbl.cy };
 
-	z = xy_to_complex(row, col);
-	for (i = 0; i < gbl.n_iteration; i++) {
+        z = xy_to_complex(row, col);
+        for (i = 0; i < gbl.n_iteration; i++) {
                 if (complex_modulus2(z) >= 4.0)
-			break;
+                        break;
                 /* "z = z^2 + c */
                 z = complex_add(complex_sq(z), c);
-	}
+        }
 
         if (i == gbl.n_iteration)
                 return INSIDE;
 
-	/* TODO: Dither here */
-	ret = (double)i;
-	if (i < gbl.n_iteration) {
-		if (gbl.dither) {
-			/* Smooth by dithering */
-			int v = rand();
-			if (v < 0)
-				v = -1 * (v & 0xff);
-			else
-				v &= 0xff;
-			double diff = (double)v / 128.0;
-			ret += diff;
-		}
+        /* TODO: Dither here */
+        ret = (double)i;
+        if (i < gbl.n_iteration) {
+                if (gbl.dither) {
+                        /* Smooth by dithering */
+                        int v = rand();
+                        if (v < 0)
+                                v = -1 * (v & 0xff);
+                        else
+                                v &= 0xff;
+                        double diff = (double)v / 128.0;
+                        ret += diff;
+                }
 
-		/* TODO: Smooth by distance */
-		if (gbl.dither) {
-			double log_zn = log10(complex_modulus2(z)) / 2.0;
-			double log_2 = log10(2.0);
-			double nu = log10(log_zn / log_2) / log_2;
-			ret += 1.0 - nu;
-		}
+                /* TODO: Smooth by distance */
+                if (gbl.dither) {
+                        double log_zn = log10(complex_modulus2(z)) / 2.0;
+                        double log_2 = log10(2.0);
+                        double nu = log10(log_zn / log_2) / log_2;
+                        ret += 1.0 - nu;
+                }
 
-		if (ret >= (double)gbl.n_iteration)
-			ret = (double)(gbl.n_iteration - 1);
-		else if (ret < 0.0)
-			ret = 0.0;
-	}
-	return ret;
+                if (ret >= (double)gbl.n_iteration)
+                        ret = (double)(gbl.n_iteration - 1);
+                else if (ret < 0.0)
+                        ret = 0.0;
+        }
+        return ret;
 }
 
 static void
 julia(void)
 {
-	int row, col;
-	unsigned long total;
-	unsigned long *histogram;
-	double *ptbuf, *tbuf;
-	histogram = malloc(gbl.n_iteration * sizeof(*histogram));
-	if (!histogram)
-		oom();
-	tbuf = malloc(gbl.width * gbl.height * sizeof(*tbuf));
-	if (!tbuf)
-		oom();
-	memset(histogram, 0, gbl.n_iteration * sizeof(*histogram));
-	total = 0;
+        int row, col;
+        unsigned long total;
+        unsigned long *histogram;
+        double *ptbuf, *tbuf;
+        histogram = malloc(gbl.n_iteration * sizeof(*histogram));
+        if (!histogram)
+                oom();
+        tbuf = malloc(gbl.width * gbl.height * sizeof(*tbuf));
+        if (!tbuf)
+                oom();
+        memset(histogram, 0, gbl.n_iteration * sizeof(*histogram));
+        total = 0;
 
-	ptbuf = tbuf;
-	for (row = 0; row < gbl.height; row++) {
-		for (col = 0; col < gbl.width; col++) {
-			double i = julia_px(row, col);
-			histogram[(int)i]++;
-			total += (int)i;
-			*ptbuf++ = i;
-		}
-	}
-	ptbuf = tbuf;
-	for (row = 0; row < gbl.height; row++) {
-		for (col = 0; col < gbl.width; col++) {
-			unsigned int color;
-			double i = *ptbuf++;
-			i += ((double)histogram[(int)i] / (double)total + 0.5);
-			color = get_color(i);
-			pxbuf_fill_pixel(gbl.pxbuf, row, col, color);
-		}
-	}
+        ptbuf = tbuf;
+        for (row = 0; row < gbl.height; row++) {
+                for (col = 0; col < gbl.width; col++) {
+                        double i = julia_px(row, col);
+                        histogram[(int)i]++;
+                        total += (int)i;
+                        *ptbuf++ = i;
+                }
+        }
+        ptbuf = tbuf;
+        for (row = 0; row < gbl.height; row++) {
+                for (col = 0; col < gbl.width; col++) {
+                        unsigned int color;
+                        double i = *ptbuf++;
+                        i += ((double)histogram[(int)i] / (double)total + 0.5);
+                        color = get_color(i);
+                        pxbuf_fill_pixel(gbl.pxbuf, row, col, color);
+                }
+        }
 }
 
 int
 main(int argc, char **argv)
 {
-	FILE *fp;
-	char *endptr;
-	int opt;
+        FILE *fp;
+        char *endptr;
+        int opt;
         const char *outfile = "julia1.bmp";
-	while ((opt = getopt(argc, argv, "dz:x:y:w:h:n:R:I:p:o:")) != -1) {
-		switch (opt) {
-		case 'd':
-			gbl.dither = true;
-			break;
-		case 'z':
-			gbl.zoom_pct = strtod(optarg, &endptr);
-			if (endptr == optarg)
-				usage();
-			break;
-		case 'x':
-			gbl.zoom_xoffs = strtod(optarg, &endptr);
-			if (endptr == optarg)
-				usage();
-			break;
-		case 'y':
-			gbl.zoom_yoffs = strtod(optarg, &endptr);
-			if (endptr == optarg)
-				usage();
-			break;
-		case 'w':
-			gbl.width = strtoul(optarg, &endptr, 0);
-			if (endptr == optarg)
-				usage();
-			break;
-		case 'h':
-			gbl.height = strtoul(optarg, &endptr, 0);
-			if (endptr == optarg)
-				usage();
-			break;
-		case 'n':
-			gbl.n_iteration = strtoul(optarg, &endptr, 0);
-			if (endptr == optarg)
-				usage();
-			break;
+        while ((opt = getopt(argc, argv, "dz:x:y:w:h:n:R:I:p:o:")) != -1) {
+                switch (opt) {
+                case 'd':
+                        gbl.dither = true;
+                        break;
+                case 'z':
+                        gbl.zoom_pct = strtod(optarg, &endptr);
+                        if (endptr == optarg)
+                                usage();
+                        break;
+                case 'x':
+                        gbl.zoom_xoffs = strtod(optarg, &endptr);
+                        if (endptr == optarg)
+                                usage();
+                        break;
+                case 'y':
+                        gbl.zoom_yoffs = strtod(optarg, &endptr);
+                        if (endptr == optarg)
+                                usage();
+                        break;
+                case 'w':
+                        gbl.width = strtoul(optarg, &endptr, 0);
+                        if (endptr == optarg)
+                                usage();
+                        break;
+                case 'h':
+                        gbl.height = strtoul(optarg, &endptr, 0);
+                        if (endptr == optarg)
+                                usage();
+                        break;
+                case 'n':
+                        gbl.n_iteration = strtoul(optarg, &endptr, 0);
+                        if (endptr == optarg)
+                                usage();
+                        break;
                 case 'o':
                         outfile = optarg;
                         break;
@@ -204,24 +204,24 @@ main(int argc, char **argv)
                         if (endptr[0] == 'p' && endptr[1] == 'i')
                                 gbl.cy *= pi;
                         break;
-		default:
-			usage();
-		}
-	}
-	gbl.pxbuf = pxbuf_create(gbl.width, gbl.height, COLOR_BLACK);
-	if (!gbl.pxbuf) {
-		fprintf(stderr, "OOM!\n");
-		return 1;
-	}
+                default:
+                        usage();
+                }
+        }
+        gbl.pxbuf = pxbuf_create(gbl.width, gbl.height, COLOR_BLACK);
+        if (!gbl.pxbuf) {
+                fprintf(stderr, "OOM!\n");
+                return 1;
+        }
 
-	fp = fopen(outfile, "wb");
-	if (!fp) {
-		fprintf(stderr, "Cannot open output file\n");
-		return 1;
-	}
-	julia();
-	pxbuf_print(gbl.pxbuf, fp);
-	fclose(fp);
-	pxbuf_free(gbl.pxbuf);
-	return 0;
+        fp = fopen(outfile, "wb");
+        if (!fp) {
+                fprintf(stderr, "Cannot open output file\n");
+                return 1;
+        }
+        julia();
+        pxbuf_print(gbl.pxbuf, fp);
+        fclose(fp);
+        pxbuf_free(gbl.pxbuf);
+        return 0;
 }
