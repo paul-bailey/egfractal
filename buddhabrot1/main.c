b9531ebc3f8779ab3542fd283308fc4d943d1bd2
pbailey <>
2018-11-27 16:37:36 -0800

    Buddhabrot1: Resolve tab/space dispute.

--- buddhabrot1/main.c
+++ buddhabrot1/main.c
@@ -10,36 +10,36 @@
 static struct gbl_t {
         Pxbuf *pxbuf;
         int n_red;
-	int n_green;
-	int n_blue;
+        int n_green;
+        int n_blue;
         int height;
         int width;
-	int min;
+        int min;
         mfloat_t zoom_pct;
-	mfloat_t zoom_pct_inv;
+        mfloat_t zoom_pct_inv;
         mfloat_t zoom_xoffs;
         mfloat_t zoom_yoffs;
         mfloat_t bailout;
         mfloat_t bailsqu;
         unsigned long points;
-	bool singlechan;
+        bool singlechan;
         bool do_hist;
         bool verbose;
 } gbl = {
         .pxbuf      = NULL,
         .n_red      = 5000,
-	.n_green    = 500,
-	.n_blue     = 50,
+        .n_green    = 500,
+        .n_blue     = 50,
         .height     = 600,
         .width      = 600,
-	.min        = 3,
+        .min        = 3,
         .zoom_pct   = 1.0,
         .zoom_xoffs = 0.0,
         .zoom_yoffs = 0.0,
         .bailsqu    = 4.0,
         .bailout    = 2.0,
         .points     = 500000,
-	.singlechan = false,
+        .singlechan = false,
         .do_hist    = false,
         .verbose    = true,
 };
@@ -55,44 +55,44 @@ oom(void)
 static void
 usage(void)
 {
-	fprintf(stderr, "Bad arg\n");
-	exit(EXIT_FAILURE);
+        fprintf(stderr, "Bad arg\n");
+        exit(EXIT_FAILURE);
 }
 
 static void
 graph_to_pixel(complex_t c, unsigned int *px, unsigned int *py)
 {
-	static const mfloat_t THIRD = 1.0 / 3.0;
+        static const mfloat_t THIRD = 1.0 / 3.0;
         /*
          * TODO: Factor in zoom.  We can't zoom for calculations,
          * but we can for final result.  This would require 7 gazillion
          * samples and would take forever, but we wouldn't be allocating
          * such a large buffer while we're doing it.
          */
-	*px = (int)((mfloat_t)gbl.width * ((c.re + 2.0) * THIRD) + 0.5);
-	*py = (int)((mfloat_t)gbl.height * ((c.im + 1.5) * THIRD) + 0.5);
+        *px = (int)((mfloat_t)gbl.width * ((c.re + 2.0) * THIRD) + 0.5);
+        *py = (int)((mfloat_t)gbl.height * ((c.im + 1.5) * THIRD) + 0.5);
 }
 
 static void
 iterate_r(complex_t c, unsigned long *buf, int n, bool isdivergent)
 {
         int i;
-	int min = gbl.min;
+        int min = gbl.min;
         complex_t z = { .re = 0.0L, .im = 0.0L };
         for (i = 0; i < n; i++) {
                 /* next z = z^2 + c */
                 complex_t ztmp = complex_add(complex_sq(z), c);
                 if (isdivergent && i > min) {
-			unsigned int drawnx, drawny;
-			graph_to_pixel(ztmp, &drawnx, &drawny);
+                        unsigned int drawnx, drawny;
+                        graph_to_pixel(ztmp, &drawnx, &drawny);
                         if (drawnx < gbl.width && drawny < gbl.height) {
                                 buf[drawny * gbl.height + drawnx]++;
                         }
                 }
 
-		/* Check both bailout and periodicity */
+                /* Check both bailout and periodicity */
                 if (complex_modulus2(ztmp) >= gbl.bailsqu
-		    || (ztmp.re == z.re && ztmp.im == z.im)) {
+                    || (ztmp.re == z.re && ztmp.im == z.im)) {
                         if (!isdivergent)
                                 iterate_r(c, buf, n, true);
                         return;
@@ -139,8 +139,8 @@ normalize(unsigned long *buf, unsigned int npx, unsigned long new_max)
         for (i = 0; i < npx; i++) {
                 /* XXX arbitrary division */
                 buf[i] = buf[i] * (new_max+1) / max;
-		if (buf[i] > new_max)
-			buf[i] = new_max;
+                if (buf[i] > new_max)
+                        buf[i] = new_max;
         }
 }
 
@@ -198,64 +198,64 @@ bbrot1(void)
         };
         int n[3] = { gbl.n_red, gbl.n_green, gbl.n_blue };
         int row, col, npct, nchan;
-	unsigned long i; /* must be wide as gbl.points */
-	unsigned long onepct = gbl.points / 100;
-	unsigned long pctcount;
+        unsigned long i; /* must be wide as gbl.points */
+        unsigned long onepct = gbl.points / 100;
+        unsigned long pctcount;
         unsigned long *buffer;
         unsigned long *chanbuf[3];
-	/* Since I use this all over */
-	unsigned int npx =  gbl.width * gbl.height;
+        /* Since I use this all over */
+        unsigned int npx =  gbl.width * gbl.height;
 
-	nchan = gbl.singlechan ? 1 : 3;
+        nchan = gbl.singlechan ? 1 : 3;
 
-	/* Initialize this */
-	gbl.zoom_pct_inv = 1.0 / gbl.zoom_pct;
+        /* Initialize this */
+        gbl.zoom_pct_inv = 1.0 / gbl.zoom_pct;
 
         buffer = malloc(npx * sizeof(*buffer) * nchan);
         if (!buffer)
                 oom();
         memset(buffer, 0, npx * sizeof(*buffer) * nchan);
-	for (i = 0; i < nchan; i++)
-        	chanbuf[i] = &buffer[npx * i];
+        for (i = 0; i < nchan; i++)
+                chanbuf[i] = &buffer[npx * i];
 
-	pctcount = npct = 0;
+        pctcount = npct = 0;
         if (gbl.verbose)
-	        printf("Progress     ");
+                printf("Progress     ");
         for (i = 0; i < gbl.points; i++) {
                 complex_t c;
                 int chan;
 
                 c.re = erand48(&seeds[0]) * 3.0 - 2.0;
                 c.im = erand48(&seeds[3]) * 3.0 - 1.5;
-		if (gbl.verbose && (pctcount++ == onepct)) {
-			npct++;
-			pctcount = 0;
-			printf("\e[3D%3d", npct);
-			fflush(stdout);
-		}
-        	for (chan = 0; chan < nchan; chan++)
-			iterate(c, chanbuf[chan], n[chan]);
+                if (gbl.verbose && (pctcount++ == onepct)) {
+                        npct++;
+                        pctcount = 0;
+                        printf("\e[3D%3d", npct);
+                        fflush(stdout);
+                }
+                for (chan = 0; chan < nchan; chan++)
+                        iterate(c, chanbuf[chan], n[chan]);
         }
         if (gbl.verbose)
-	        putchar('\n');
+                putchar('\n');
 
-	for (i = 0; i < nchan; i++) {
+        for (i = 0; i < nchan; i++) {
                 if (gbl.do_hist)
                         hist_eq(chanbuf[i], npx);
-		normalize(chanbuf[i], npx, 255);
-	}
+                normalize(chanbuf[i], npx, 255);
+        }
 
         i = 0;
         for (row = 0; row < gbl.height; row++) {
                 for (col = 0; col < gbl.width; col++) {
                         unsigned int r, g, b;
                         r = chanbuf[0][i];
-			if (nchan > 1) {
-                        	g = chanbuf[1][i];
-                        	b = chanbuf[2][i];
-			} else {
-				g = b = r;
-			}
+                        if (nchan > 1) {
+                                g = chanbuf[1][i];
+                                b = chanbuf[2][i];
+                        } else {
+                                g = b = r;
+                        }
                         i++;
 
                         /*
@@ -279,12 +279,12 @@ bbrot1(void)
 int
 main(int argc, char **argv)
 {
-	FILE *fp;
-	char *endptr;
-	int opt;
+        FILE *fp;
+        char *endptr;
+        int opt;
         char *outfile = "buddhabrot1.bmp";
-	while ((opt = getopt(argc, argv, "HB:b:g:h:m:o:p:r:svw:x:y:z:")) != -1) {
-		switch (opt) {
+        while ((opt = getopt(argc, argv, "HB:b:g:h:m:o:p:r:svw:x:y:z:")) != -1) {
+                switch (opt) {
                 case 'B':
                         gbl.bailout = strtold(optarg, &endptr);
                         if (endptr == optarg)
@@ -294,80 +294,80 @@ main(int argc, char **argv)
                 case 'H':
                         gbl.do_hist = true;
                         break;
-		case 'b':
-			gbl.n_blue = strtoul(optarg, &endptr, 0);
-			if (endptr == optarg)
-				usage();
-			break;
-		case 'g':
-			gbl.n_green = strtoul(optarg, &endptr, 0);
-			if (endptr == optarg)
-				usage();
-			break;
-		case 'h':
-			gbl.height = strtoul(optarg, &endptr, 0);
-			if (endptr == optarg)
-				usage();
-			break;
-		case 'm':
-			gbl.min = strtoul(optarg, &endptr, 0);
-			if (endptr == optarg)
-				usage();
-			break;
+                case 'b':
+                        gbl.n_blue = strtoul(optarg, &endptr, 0);
+                        if (endptr == optarg)
+                                usage();
+                        break;
+                case 'g':
+                        gbl.n_green = strtoul(optarg, &endptr, 0);
+                        if (endptr == optarg)
+                                usage();
+                        break;
+                case 'h':
+                        gbl.height = strtoul(optarg, &endptr, 0);
+                        if (endptr == optarg)
+                                usage();
+                        break;
+                case 'm':
+                        gbl.min = strtoul(optarg, &endptr, 0);
+                        if (endptr == optarg)
+                                usage();
+                        break;
                 case 'o':
                         outfile = optarg;
                         break;
-		case 'p':
-			gbl.points = strtoul(optarg, &endptr, 0);
-			if (endptr == optarg)
-				usage();
-			break;
-		case 'r':
-			gbl.n_red = strtoul(optarg, &endptr, 0);
-			if (endptr == optarg)
-				usage();
-			break;
-		case 's':
-			gbl.singlechan = true;
-			break;
+                case 'p':
+                        gbl.points = strtoul(optarg, &endptr, 0);
+                        if (endptr == optarg)
+                                usage();
+                        break;
+                case 'r':
+                        gbl.n_red = strtoul(optarg, &endptr, 0);
+                        if (endptr == optarg)
+                                usage();
+                        break;
+                case 's':
+                        gbl.singlechan = true;
+                        break;
                 case 'v':
                         gbl.verbose = true;
                         break;
-		case 'w':
-			gbl.width = strtoul(optarg, &endptr, 0);
-			if (endptr == optarg)
-				usage();
-			break;
-		case 'x':
-			gbl.zoom_xoffs = strtold(optarg, &endptr);
-			if (endptr == optarg)
-				usage();
-			break;
-		case 'y':
-			gbl.zoom_yoffs = strtold(optarg, &endptr);
-			if (endptr == optarg)
-				usage();
-			break;
-		case 'z':
-			gbl.zoom_pct = strtold(optarg, &endptr);
-			if (endptr == optarg)
-				usage();
-			break;
-		default:
-			usage();
-		}
-	}
-
-	/* One quick sanity check */
-        if (gbl.min >= gbl.n_red) {
-		fprintf(stderr, "min too high!\n");
-		return 1;
+                case 'w':
+                        gbl.width = strtoul(optarg, &endptr, 0);
+                        if (endptr == optarg)
+                                usage();
+                        break;
+                case 'x':
+                        gbl.zoom_xoffs = strtold(optarg, &endptr);
+                        if (endptr == optarg)
+                                usage();
+                        break;
+                case 'y':
+                        gbl.zoom_yoffs = strtold(optarg, &endptr);
+                        if (endptr == optarg)
+                                usage();
+                        break;
+                case 'z':
+                        gbl.zoom_pct = strtold(optarg, &endptr);
+                        if (endptr == optarg)
+                                usage();
+                        break;
+                default:
+                        usage();
+                }
         }
 
-	if (!gbl.singlechan && (gbl.min >= gbl.n_green || gbl.min >= gbl.n_blue)) {
-		fprintf(stderr, "min too high!\n");
-		return 1;
-	}
+        /* One quick sanity check */
+        if (gbl.min >= gbl.n_red) {
+                fprintf(stderr, "min too high!\n");
+                return 1;
+        }
+
+        if (!gbl.singlechan && (gbl.min >= gbl.n_green || gbl.min >= gbl.n_blue)) {
+                fprintf(stderr, "min too high!\n");
+                return 1;
+        }
 
         gbl.pxbuf = pxbuf_create(gbl.width, gbl.height, COLOR_WHITE);
         if (!gbl.pxbuf)
