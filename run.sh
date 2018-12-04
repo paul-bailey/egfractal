88a529fd4ccdf084489ef62913095fb4d09c6701
pbailey <>
2018-12-04 14:51:25 -0800

    Further simplification of commands in run.sh

--- run.sh
+++ run.sh
@@ -47,34 +47,36 @@ fi
 
 test -d ${outdir} || mkdir ${outdir}
 
-common_args="${size_opt} ${verbose} -o ${outdir}/mandelbrot-favorites"
-./mandelbrot/mandelbrot ${common_args}-01.bmp -b32768 -z1.0e-4  -d1 -p1 -x 1.2090000 -y0.2385000
-./mandelbrot/mandelbrot ${common_args}-02.bmp -b32768 -z1.0e-7  -d1 -p2 -x 1.20899252 -y0.2385098 -n1400
-./mandelbrot/mandelbrot ${common_args}-03.bmp -b32768 -z5.0e-8  -d1 -p6 -x-0.1550495 -y-0.65059865
-./mandelbrot/mandelbrot ${common_args}-04.bmp -b65536 -z4.0e-12 -d3 -p3 -x-0.14000524460488 -y-0.64935985788190
-./mandelbrot/mandelbrot ${common_args}-05.bmp -b32768 -z1.0e-4 -D -x 0.76991000 -y 0.10949000
-./mandelbrot/mandelbrot ${common_args}-06.bmp -b32768 -z1.0e-6 -D -x-0.25204350 -y 0.00014850 -n3000 --negate
-./mandelbrot/mandelbrot ${common_args}-07.bmp -b32768 -z1.0e-6 -D -x-0.25205250 -y 0.00014590 -n100000
-./mandelbrot/mandelbrot ${common_args}-08.bmp -b32768 -z5.0e-7 -D -x-0.25205185 -y 0.00014800 -n100000
-./mandelbrot/mandelbrot ${common_args}-09.bmp -b32768 -z5.0e-6 -D -x-0.25205000 -y 0.00014850 -n10000
-./mandelbrot/mandelbrot ${common_args}-10.bmp -b32768 -z1.0e-3 -x 0.77000000 -y 0.11000000 --distance=6
-./mandelbrot/mandelbrot ${common_args}-11.bmp -b65536 -z1.0e-3 -D -x 0.77000000 -y 0.11000000 --color-distance -p2 --negate
-./mandelbrot/mandelbrot ${common_args}-12.bmp -b65536 -z4 --formula sin --distance=8 --color-distance -p4
-./mandelbrot/mandelbrot ${common_args}-13.bmp -b32768 -z 0.0010 --formula burnship -p1 -x 1.625 -y-0.00200
-./mandelbrot/mandelbrot ${common_args}-14.bmp -b32768 -z 0.0010 --formula burnship -p1 -x 1.620 -y 0.00199
-./mandelbrot/mandelbrot ${common_args}-15.bmp -b32768 -z 0.0005 --formula burnship -p1 -x 1.624 -y-0.00100
+common_args="${size_opt} ${verbose}"
 
-common_args="${size_opt} ${verbose} -o ${outdir}/julia1"
-./julia1/julia1 ${common_args}-1a.bmp -p2 -R-0.701760000 -I-0.3842000 -b32768 -d1
-./julia1/julia1 ${common_args}-1b.bmp -p1 -R-0.400000000 -I-0.6000000 -b32768 -d1
-./julia1/julia1 ${common_args}-1c.bmp -p1 -R-0.209600000 -I 0.7904000 -b32768 -d1 -z0.01 -x0.1
-./julia1/julia1 ${common_args}-1d.bmp -D --equalize=0.8 -p1 -R-0.209600000 -I 0.7904000 -b32768 -d1 -z0.01 -x0.1
-./julia1/julia1 ${common_args}-1e.bmp -p2 -R-0.200000000 -I 0.8000000 -b32768 -d1
-./julia1/julia1 ${common_args}-1f.bmp -p2 -R-0.200000000 -I 0.8000000 -b32768 -d1 -z0.1
-./julia1/julia1 ${common_args}-1g.bmp -p2 -R 0.138565244 -I-0.6493599 -z1.0e-3 -x0.1206000 -y0.5230000
-./julia1/julia1 ${common_args}-1h.bmp -p2 -R 0.138565244 -I-0.6493599 -z5.0e-5 -x0.1205994 -y0.5231718
-./julia1/julia1 ${common_args}-1i.bmp -p2 -R-0.701760000 -I-0.3842000 -b32768 -d1 --negate -n1500
-./julia1/julia1 ${common_args}-1j.bmp -p1 -R 1.625000000 -I-0.0020000 -b32768 --formula burnship -D --color-distance
+mbrot="./mandelbrot/mandelbrot ${common_args} -o ${outdir}/mandelbrot-favorites"
+${mbrot}-01.bmp -b32768 -z1.0e-4  -d1 -p1 -x 1.2090000 -y0.2385000
+${mbrot}-02.bmp -b32768 -z1.0e-7  -d1 -p2 -x 1.20899252 -y0.2385098 -n1400
+${mbrot}-03.bmp -b32768 -z5.0e-8  -d1 -p6 -x-0.1550495 -y-0.65059865
+${mbrot}-04.bmp -b65536 -z4.0e-12 -d3 -p3 -x-0.14000524460488 -y-0.64935985788190
+${mbrot}-05.bmp -b32768 -z1.0e-4 -D -x 0.76991000 -y 0.10949000
+${mbrot}-06.bmp -b32768 -z1.0e-6 -D -x-0.25204350 -y 0.00014850 -n3000 --negate
+${mbrot}-07.bmp -b32768 -z1.0e-6 -D -x-0.25205250 -y 0.00014590 -n100000
+${mbrot}-08.bmp -b32768 -z5.0e-7 -D -x-0.25205185 -y 0.00014800 -n100000
+${mbrot}-09.bmp -b32768 -z5.0e-6 -D -x-0.25205000 -y 0.00014850 -n10000
+${mbrot}-10.bmp -b32768 -z1.0e-3 -x 0.77000000 -y 0.11000000 --distance=6
+${mbrot}-11.bmp -b65536 -z1.0e-3 -D -x 0.77000000 -y 0.11000000 --color-distance -p2 --negate
+${mbrot}-12.bmp -b65536 -z4 --formula sin --distance=8 --color-distance -p4
+${mbrot}-13.bmp -b32768 -z 0.0010 --formula burnship -p1 -x 1.625 -y-0.00200
+${mbrot}-14.bmp -b32768 -z 0.0010 --formula burnship -p1 -x 1.620 -y 0.00199
+${mbrot}-15.bmp -b32768 -z 0.0005 --formula burnship -p1 -x 1.624 -y-0.00100
+
+julia="./julia1/julia1 ${common_args} -o ${outdir}/julia1"
+${julia}-1a.bmp -p2 -R-0.701760000 -I-0.3842000 -b32768 -d1
+${julia}-1b.bmp -p1 -R-0.400000000 -I-0.6000000 -b32768 -d1
+${julia}-1c.bmp -p1 -R-0.209600000 -I 0.7904000 -b32768 -d1 -z0.01 -x0.1
+${julia}-1d.bmp -D --equalize=0.8 -p1 -R-0.209600000 -I 0.7904000 -b32768 -d1 -z0.01 -x0.1
+${julia}-1e.bmp -p2 -R-0.200000000 -I 0.8000000 -b32768 -d1
+${julia}-1f.bmp -p2 -R-0.200000000 -I 0.8000000 -b32768 -d1 -z0.1
+${julia}-1g.bmp -p2 -R 0.138565244 -I-0.6493599 -z1.0e-3 -x0.1206000 -y0.5230000
+${julia}-1h.bmp -p2 -R 0.138565244 -I-0.6493599 -z5.0e-5 -x0.1205994 -y0.5231718
+${julia}-1i.bmp -p2 -R-0.701760000 -I-0.3842000 -b32768 -d1 --negate -n1500
+${julia}-1j.bmp -p1 -R 1.625000000 -I-0.0020000 -b32768 --formula burnship -D --color-distance
 
 if test $convert = y
         then
